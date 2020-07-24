2a3f891d661746dcd724b47bdc76af140b46fe4c
Ashley Howard <me@ashley.how>
2020-07-24 15:54:15 +0100

    SITE UPGRADE 2021 (dev-mode)

---
+++ scripts/typed.js
@@ -0,0 +1,7 @@
+if (document.getElementById('typed')) {
+    var typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        showCursor: false,
+        typeSpeed: 50
+    });
+}
