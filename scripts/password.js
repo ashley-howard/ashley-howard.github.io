7a05e21d2b75cecbbf616f447a55e7c098b6a729
Ashley Howard <me@ashley.how>
2020-07-25 12:14:07 +0100

    update password

--- scripts/password.js
+++ scripts/password.js
@@ -2,11 +2,13 @@ var devPass = localStorage.getItem('dev') ? localStorage.getItem('dev') : 'no';
 
 if (localStorage.getItem('dev') == 'yes') {
     document.getElementById('password').style.display = "none"
+    document.body.style.overflow = 'auto'
 }
 
 function passwordSubmit() {
     if (document.getElementById('password-text').value == "dev-only" && !localStorage.getItem('dev')) {
         document.getElementById('password').style.display = "none"
+        document.body.style.overflow = 'auto'
         localStorage.setItem('dev', 'yes');
     }
 }
