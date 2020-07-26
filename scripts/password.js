8312f179bf01955d65f45e11a55bb0ea43c68eb6
Ashley Howard <me@ashley.how>
2020-07-26 12:45:09 +0100

    fix header position and logo

--- scripts/password.js
+++ scripts/password.js
@@ -1,12 +1,12 @@
-var devPass = localStorage.getItem('has_access') ? localStorage.getItem('has_access') : 'no';
+var hasAccess = localStorage.getItem('has_access') ? localStorage.getItem('has_access') : 'no';
 
-if (localStorage.getItem('has_access') == 'yes') {
+if (hasAccess == 'yes') {
     document.getElementById('password').style.display = "none"
     document.body.style.overflow = 'auto'
 }
 
 function passwordSubmit() {
-    if (document.getElementById('password-text').value == "dev-only" && !localStorage.getItem('has_access')) {
+    if (document.getElementById('password-text').value == "dev-only" && hasAccess == "no") {
         document.getElementById('password').style.display = "none"
         document.body.style.overflow = 'auto'
         localStorage.setItem('has_access', 'yes');
