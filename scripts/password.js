7542d8dbaa8571f0da40d57af6b97adf4227adb8
Ashley <38157300+ashley-howard@users.noreply.github.com>
2020-10-29 14:30:04 +0000

    Update password.js

--- scripts/password.js
+++ scripts/password.js
@@ -1,14 +1,14 @@
-//*var hasAccess = localStorage.getItem('has_access') ? localStorage.getItem('has_access') : 'no';
+var hasAccess = localStorage.getItem('has_access') ? localStorage.getItem('has_access') : 'no';
 
-// if (hasAccess == 'yes') {
-//     document.getElementById('password').style.display = "none"
-//     document.body.style.overflow = 'auto'
-// }
+if (hasAccess == 'yes') {
+    document.getElementById('password').style.display = "none"
+    document.body.style.overflow = 'auto'
+}
 
-// function passwordSubmit() {
-//     if (document.getElementById('password-text').value == "dev-only" && hasAccess == "no") {
-//         document.getElementById('password').style.display = "none"
-//         document.body.style.overflow = 'auto'
-//         localStorage.setItem('has_access', 'yes');
-//     }
-// }*
+function passwordSubmit() {
+    if (document.getElementById('password-text').value == "dev-only" && hasAccess == "no") {
+        document.getElementById('password').style.display = "none"
+        document.body.style.overflow = 'auto'
+        localStorage.setItem('has_access', 'yes');
+    }
+}
